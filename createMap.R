c72c86cec26b53cc96262cf5048ca9c12deecd5d
Josh Drizin <josh@respectablename.com>
2013-06-13 14:08:56 -0500

    initial commit of my first map trial

---
+++ createMap.R
@@ -0,0 +1,8 @@
+
+
+# load necessary packages
+# ggmap - mapping
+# maptools - might need it. provides conversions for geospatial data
+# raster - google suggests it could be useful?
+
+library(ggmap) # loads ggplot2
