c219308eabdba600349c95e16eebc0b61dd6ccbd
Josh Drizin <josh@respectablename.com>
2013-07-10 15:00:42 -0500

    fix points, merge a new bit of data

--- georeferencing.R
+++ georeferencing.R
@@ -44,8 +44,6 @@ printMap # i guess the default method is to print a plot.
 #### let's try a somewhat more clever approach to geocoding ####
 # just countries
 dummyvec <- rep(0, length(arto$index) ) #make a dummy vector
-arto$countryLat <- dummyvec             #add it as latlongs
-arto$countryLon <- dummyvec
 countries <- unique(sort(arto$Country)) #looks like we have 20 countries
 countryLocs <- data.frame(countries, lat=rep(0,20), lon=rep(0,20), stringsAsFactors=F) #fill out an empty data frame
 
@@ -55,3 +53,41 @@ for(i in 1:length(countries)){
 	countryLocs$lon[i] <- loc$lon
 }
 
+#there are some weird points around
+
+loc <- geocode("thailand") #listed as siam, but seemed to be somewhere weird 
+countryLocs$lat[18] <- loc$lat
+countryLocs$lon[18] <- loc$lon
+
+loc <- geocode("java, indonesia") #listed as java, but seemed to be somewhere weird 
+countryLocs$lat[9] <- loc$lat
+countryLocs$lon[9] <- loc$lon
+
+loc <- geocode("philippines") #philippine islands doesn't work. oooooook.
+countryLocs$lat[16] <- loc$lat
+countryLocs$lon[16] <- loc$lon
+
+loc <- geocode("peninsular malaysia") #and i had to decide where malaya means.
+countryLocs$lat[12] <- loc$lat
+countryLocs$lon[12] <- loc$lon
+
+##### build test map: how did it do? #####
+mapCenter <- geocode("Sandakan, Malaysia") # this is about right
+mapSize <- 3 # 3-21, continent to zoom, 10 is city.
+
+distMap <- get_map(location=c(lon=mapCenter$lon, lat=mapCenter$lat), zoom=mapSize, maptype="satellite")
+printMap <- ggmap(distMap) + 
+	geom_point(data=countryLocs, aes(x=lon, y=lat)) +
+	geom_text (data=countryLocs, aes(x=lon, y=lat, label=countries))
+	# might be nice to add some cities too
+printMap # no out-of-bounds points.
+
+# ok, so countryLocs now seems to have good mappings for countries -> latlong
+# now it's time to populate the original table with this.
+# begin merge()
+names(countryLocs)[2] <- "countryLat" #fix the names
+names(countryLocs)[3] <- "countryLon"
+arto <- merge(arto, countryLocs, by.x="Country", by.y="countries")
+
+#all merged. let's write the data to a new file
+write.csv(arto, file="data/jarret-country.csv")
